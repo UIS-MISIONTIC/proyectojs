aa1f47d172a30402f306700387cb1a0db4af6aa4
LuisLora334 <69110776+VuelaFacilGrupo4@users.noreply.github.com>
2021-11-12 18:21:20 -0500

    ConexionFireBase

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+const admin = require('firebase-admin');
+
+
+const serviceAccount = require('./vuelafacil2021-fd081e754bb2.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+
+
+db.collection("users").add({
+  first: "Ada",
+  last: "Lovelace",
+  born: 1815
+})
+.then((docRef) => {
+  console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+  console.error("Error adding document: ", error);
+});
+
+
 var app = require('./app');
 var port = 4000;
 app.listen(port, () =>{
