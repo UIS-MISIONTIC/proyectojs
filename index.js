1709ab98bbab40d032c9fd59f71beb6cd293fb1c
LuisLora334 <lelora3@misena.edu.co>
2021-11-13 07:56:13 -0500

    ReadDelete BE

--- index.js
+++ index.js
@@ -49,18 +49,62 @@ const db = admin.firestore();
 
 //Update a document
 
-db.collection("ciudad").doc("01").update({
-    ciAeropuerto: "Aeropuerto de Rio Negro",
-    ciNombre: "Medellin",
-    ciPais: "Colombia"
- })
-.then(() => {
-    console.log("Document successfully updated!");
-})
-.catch((error) => {
-    // The document probably doesn't exist.
-    console.error("Error updating document: ", error);
-});
+// db.collection("ciudad").doc("01").update({
+//     ciAeropuerto: "Rio Negro"
+//  })
+// .then(() => {
+//     console.log("Document successfully updated!");
+// })
+// .catch((error) => {
+//     // The document probably doesn't exist.
+//     console.error("Error updating document: ", error);
+// });
+
+//Read a document
+
+// db.collection("ciudad").doc("01").get().then((doc) => {
+//     if (doc.exists) {
+//         console.log("Document data:", doc.data());
+//     } else {
+//         // doc.data() will be undefined in this case
+//         console.log("No such document!");
+//     }
+// }).catch((error) => {
+//     console.log("Error getting document:", error);
+// });
+
+//Read several documents
+
+// db.collection("ciudad").where("ciPais", "==", "Colombia").get()
+
+//     .then((docArray) => {
+//         docArray.forEach((doc) => {
+//             // doc.data() is never undefined for query doc Arrays
+//             console.log(doc.id, " => ", doc.data());
+//         });
+//     })
+//     .catch((error) => {
+//         console.log("Error getting documents: ", error);
+//     });
+
+//Read all of documents
+
+    // db.collection("users").get()
+    // .then((docArray) => {
+    //     docArray.forEach((doc) => {
+    //         // doc.data() is never undefined for query doc Arrays
+    //         console.log(doc.id, " => ", doc.data());
+    //     });
+    // });
+
+//Remove a document
+
+// db.collection("users").doc("N8yfy8KcVCFsmChgwWti").delete().then(() => {
+//     console.log("Document successfully deleted!");
+// }).catch((error) => {
+//     console.error("Error removing document: ", error);
+// });
+
 var app = require('./app');
 var port = 4000;
 app.listen(port, () =>{
