18a3ac97a179b74b2e9bd6baa556068b5ab4fd6b
LuisLora334 <lelora3@misena.edu.co>
2021-11-13 00:07:50 -0500

    CreateUpdate from BE

--- index.js
+++ index.js
@@ -9,24 +9,60 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+//Create a document
+
+// db.collection("users").add({
+//   first: "Ada",
+//   last: "Lovelace",
+//   born: 1815
+// })
+// .then((docRef) => {
+//   console.log("Document written with ID: ", docRef.id);
+// })
+// .catch((error) => {
+//   console.error("Error adding document: ", error);
+// });
 
 
-db.collection("users").add({
-  first: "Ada",
-  last: "Lovelace",
-  born: 1815
-})
-.then((docRef) => {
-  console.log("Document written with ID: ", docRef.id);
+// Add a new document in collection "ciudad" 
+
+//Si el documento no existe, se creará. Si el documento existe, su contenido se reemplazará con los datos proporcionados, 
+//a menos que especifiques que los datos se deberían combinar en el documento existente, de la siguiente manera:
+
+// var cityRef = db.collection('ciudad').doc('BJ');
+
+// var setWithMerge = cityRef.set({
+//     capital: true
+// }, { merge: true });
+
+// db.collection("ciudad").doc("01").set({
+//     ciAeropuerto: "Rio Negro",
+//     ciNombre: "Medellin",
+//     ciPais: "Colombia"
+// })
+// .then(() => {
+//     console.log("Document successfully written!");
+// })
+// .catch((error) => {
+//     console.error("Error writing document: ", error);
+// });
+
+//Update a document
+
+db.collection("ciudad").doc("01").update({
+    ciAeropuerto: "Aeropuerto de Rio Negro",
+    ciNombre: "Medellin",
+    ciPais: "Colombia"
+ })
+.then(() => {
+    console.log("Document successfully updated!");
 })
 .catch((error) => {
-  console.error("Error adding document: ", error);
+    // The document probably doesn't exist.
+    console.error("Error updating document: ", error);
 });
-
-
 var app = require('./app');
 var port = 4000;
 app.listen(port, () =>{
     console.log("Server is running ok");
-
 });
