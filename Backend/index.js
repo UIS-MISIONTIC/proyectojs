8de85ee4ea2a85562a25dde312845f2b858e3ceb
LuisLora334 <lelora3@misena.edu.co>
2021-11-15 02:05:06 -0500

    Get (read) data on GUI

--- Backend/index.js
+++ Backend/index.js
@@ -10,9 +10,19 @@ app.get('/Welcome', function (req, res) {
     res.send('Bienvenido a la pagina oficial de aerolineas vuelaFacil')
   });
 
-  app.get('/Any-Interface', function (req, res) {
-    res.send('This is for getting any interface')
+app.get('/Any-Interface', function (req, res) {
+res.send('This is for getting any interface')
   });
+
+app.get('/getAerolinea', function(req, res){
+
+  db.getAerolinea(function(arrayAerolinea){
+
+    res.send(arrayAerolinea);
+
+  })
+
+});
    
 //app.listen(3000);
 //   console.log("Server is running ok");
--- Backend/src/db/crud.js
+++ Backend/src/db/crud.js
@@ -2,20 +2,24 @@ const db = require('./firebase.js');
 
 // Read all of documents from a collection
 
-function getAerolinea(){
+function getAerolinea(callback){
 
     return db.collection("ciudad").get()
     .then((refDoc) => {
+
+        var arrayAerolinea = [];
         refDoc.forEach((doc) => {
+            arrayAerolinea.push(doc.data());
             // doc.data() is never undefined for query doc Arrays
-            console.log(doc.id, " => ", doc.data());
+            // console.log(doc.id, " => ", doc.data());
         })
+        callback(arrayAerolinea);
     })
     .catch(err => {
-            console.error(`Error getting documents: ${err}`);
+            //console.error(`Error getting documents: ${err}`);
+            callback(`Error getting documents: ${err}`);
         });
 }
-
 module.exports = {
 
     getAerolinea
